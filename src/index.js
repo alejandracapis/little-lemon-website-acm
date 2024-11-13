8cb028fb490a7dd21e8ee5e44d449a8697639057
alejandracapis <acapistran17@gmail.com>
2024-11-13 15:29:50 -0600

    V9

--- src/index.js
+++ src/index.js
@@ -4,18 +4,20 @@ import './index.css';
 import App from './App';
 import '@fontsource/markazi-text';
 import '@fontsource/karla';
-import { HashRouter } from 'react-router-dom';
+import {
+      createHashRouter,
+      RouterProvider
+    } from 'react-router-dom';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+    const router = createHashRouter([
+      {
+        path: "/*",
+        element: <App />,
+      }
+    ]);
+ReactDOM.createRoot(document.getElementById('root')).render(
       <React.StrictMode>
-            <HashRouter>
-            <App />
-            </HashRouter>
+            <RouterProvider router={router} />
       </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
